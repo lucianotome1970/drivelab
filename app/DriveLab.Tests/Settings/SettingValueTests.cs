4282f548cbe5fc387c78e0a4237407a3afc3aaac
lucianotome1970 <lucianotome1970@gmail.com>
2026-07-12 20:49:59 -0300

    feat(settings): SettingType + SettingValue encode/decode

---
+++ app/DriveLab.Tests/Settings/SettingValueTests.cs
@@ -0,0 +1,37 @@
+using DriveLab.Core.Settings;
+
+namespace DriveLab.Tests.Settings;
+
+public class SettingValueTests
+{
+    [Theory]
+    [InlineData(SettingType.UInt8, 200, 1)]
+    [InlineData(SettingType.Int8, -1, 1)]
+    [InlineData(SettingType.UInt16, 900, 2)]
+    [InlineData(SettingType.Int16, -10000, 2)]
+    public void Integer_Values_RoundTrip(SettingType type, double value, int expectedBytes)
+    {
+        var setting = new SettingValue(type, value);
+        Span<byte> buffer = stackalloc byte[8];
+
+        var written = setting.WriteValue(buffer);
+        var parsed = SettingValue.ReadValue(type, buffer);
+
+        Assert.Equal(expectedBytes, written);
+        Assert.Equal(value, parsed.AsDouble);
+        Assert.Equal(type, parsed.Type);
+    }
+
+    [Fact]
+    public void Float_Value_RoundTrips()
+    {
+        var setting = new SettingValue(SettingType.Float, 0.05);
+        Span<byte> buffer = stackalloc byte[8];
+
+        var written = setting.WriteValue(buffer);
+        var parsed = SettingValue.ReadValue(SettingType.Float, buffer);
+
+        Assert.Equal(4, written);
+        Assert.Equal(0.05, parsed.AsDouble, precision: 5);
+    }
+}
